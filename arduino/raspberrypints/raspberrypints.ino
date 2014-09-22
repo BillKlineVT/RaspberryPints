c3e53d623e9103d4f0ce0b1b5ca318837800f354
BillKlineVT <bill.klinefelter@gmail.com>
2014-09-21 21:53:16 -0500

    Update raspberrypints.ino
    Replaced Hardware Serial link with an RF24Network link to the host node (RaspberryPi)

--- arduino/raspberrypints/raspberrypints.ino
+++ arduino/raspberrypints/raspberrypints.ino
@@ -1,7 +1,36 @@
+//Updated to use RF24Network instead of serial
+#include <avr/pgmspace.h>
+#include <RF24Network.h>
+#include <RF24.h>
+#include <SPI.h>
+#include <Tictocs.h>
+#include <Button.h>
+#include <TictocTimer.h>
+#include <nodeconfig.h>
+#include <sleep.h>
+#include <S_message.h>
+#include <printf.h>
+
+
+// Pins for radio
+const int rf_ce = 8;
+const int rf_csn = 9;
+
+RF24 radio(rf_ce,rf_csn);
+RF24Network network(radio);
+
+// Our node configuration 
+//eeprom_info_t this_node;
+const uint8_t this_node = 01;
+
+// Number of packets we've failed to send since we last sent one
+// successfully
+uint16_t lost_packets = 0;
+
 //This line is the number of flow sensors connected.
 const uint8_t numSensors = 4;
 //This line initializes an array with the pins connected to the flow sensors
-uint8_t pulsePin[] = {8,9,10,11};
+uint8_t pulsePin[] = {7,6,5,4};
 //number of milliseconds to wait after pour before sending message
 unsigned int pourMsgDelay = 300;
 
@@ -15,12 +44,40 @@ int lastPinState[numSensors];
 unsigned long lastSend = 0;
 
 void setup() {
-  pinMode(13, OUTPUT);
-  // initialize serial communications at 9600 bps:
-  Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for Leonardo only
-  }
+  
+  //
+  // Print preamble
+  //
+  
+  Serial.begin(57600);
+  printf_begin();
+
+  //
+  // Bring up the RF network
+  //
+
+  SPI.begin();
+  radio.begin();
+  network.begin(/*channel*/ 90, /*node address*/ this_node);
+  
+  //
+  // Request configuration from parent
+  //
+
+  RF24NetworkHeader header(network.parent(),'P');
+  
+  printf_P(PSTR("%lu: APP Sending type-%c to 0%o...\n\r"),millis(),header.type,header.to_node);
+  if ( ! network.write(header,NULL,0) )
+    printf_P(PSTR("Failed.\r\n"));
+ 
+  //testing
+  // Pump the network regularly
+  //network.update();
+  //Serial.print("7");
+  //sendPulseCount(0, 7, 267);
+  //delay(1000);
+
+  
   Serial.flush();
   for( int i = 0; i < numSensors; i++ ) {
     pinMode(pulsePin[i], INPUT);
