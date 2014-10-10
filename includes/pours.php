351dd8b2220dd929d1beae64aa6b5b5306256918
BillKlineVT <bill.klinefelter@gmail.com>
2014-10-10 11:49:35 -0500

    Fixed DB insert to include createdDate timestamp
    This will help with sorting out phantom pours as well as data analytics

--- includes/pours.php
+++ includes/pours.php
@@ -47,7 +47,7 @@ if($db){
 		//$qry = "INSERT INTO pours(tapId,amountPoured,batchId,pinAddress,pulseCount,pulsesPerLiter,liters) values ('".$taps[0]."','".$amount."','".$taps[1]."','".$PIN."','".$PULSE_COUNT."','".$taps[2]."','".$PULSE_COUNT / $taps[2]."')";
 
 		// Inserts in to the pours table 
-		$qry = "INSERT INTO pours(tapId, pinId, amountPoured, pulses) values ('".$taps[0]."','".$PIN."','".$amount."','".$PULSE_COUNT."')";
+		$qry = "INSERT INTO pours(tapId, pinId, amountPoured, pulses, createdDate) values ('".$taps[0]."','".$PIN."','".$amount."','".$PULSE_COUNT."', NOW())";
 		mysql_query($qry);
 	
 	
